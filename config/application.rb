91982de0d02ab6bb1d99ebbda76bac164b78a0b6
dsn3 <dsn3.limport@gmail.com>
2019-04-15 13:34:51 -0300

    generating portfolios

--- config/application.rb
+++ config/application.rb
@@ -29,5 +29,12 @@ module DissectingRails
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+    config.generators do |g|
+      g.orm             :active_record
+      g.template_engine :erb
+      g.test_framework  :test_unit, fixture: false
+      g.stylesheets     false
+      g.javascript      false
+    end
   end
 end
